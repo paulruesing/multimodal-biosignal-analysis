a3861b663273bf7b462b53399c4e8824e1985b2d
paulruesing <pr@paulruesing.de>
2025-11-19 10:55:35 +0100

    data resampling method

--- tests/test_signal_features.py
+++ tests/test_signal_features.py
@@ -6,8 +6,8 @@ import src.pipeline.signal_features as features
 class TestSignalFeatures:
     def setup_method(self):
         # Create dummy input data with shape (timesteps, channels)
-        timesteps = 1000; channels = 64
-        self.input_data = np.random.randn(timesteps, channels)  # 1000 timesteps, 8 channels
+        self.n_timesteps = 1000; self.n_channels = 64
+        self.input_data = np.random.randn(self.n_timesteps, self.n_channels)  # 1000 timesteps, 8 channels
         self.sampling_frequency = 500
 
     def test_compute_spectral_snr(self):
@@ -21,5 +21,17 @@ class TestSignalFeatures:
                                                    self.sampling_frequency)
 
         assert input_snr == scaled_snr, "SNR is not scale-invariant as should be."
+
+    def test_resample_data(self):
+        """ Should return same n_channels as provided and altered n_timesteps. """
+        resampling_factor = 2  # n_timesteps should increase proportionately
+
+        # call function:
+        result = features.resample_data(self.input_data, axis=0,
+                               original_sampling_freq=self.sampling_frequency,
+                               new_sampling_freq=self.sampling_frequency*resampling_factor)
+        assert result.shape[1] == self.n_channels, "Number of channels mustn't be altered during resampling."
+        assert result.shape[0] == self.n_timesteps * resampling_factor, "Number of timesteps should increase proportionately to new_sampling_freq/original_sampling_freq."
+
 
     # todo: test equivalent input file format for all methods
