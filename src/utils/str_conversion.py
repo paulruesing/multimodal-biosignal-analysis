aefc0e3188cc27f6183c1c4bc8f5a5f9fc11d576
paulrusing <pr@paulruesing.de>
2025-12-09 19:55:10 +0100

    enter line breaks for width restriction

--- src/utils/str_conversion.py
+++ src/utils/str_conversion.py
@@ -11,3 +11,18 @@ def str_to_float(input: str, is_ger_format=True) -> float:
     if input == '': return 0.0
     else:
         return float(input.strip())
+
+
+def enter_line_breaks(input_str: str, line_break_every: int = 110, max_excess_letters: int = 15) -> str:
+    if len(input_str) < line_break_every: return input_str
+    output_str = ""
+    last_break = 0
+    for break_ind in range(0, len(input_str), line_break_every):
+        for increment in range(max_excess_letters):  # find next whitespace
+            try:
+                if input_str[break_ind + line_break_every + increment] == " ":
+                    end_break = break_ind + line_break_every + increment
+            except IndexError: end_break = break_ind + line_break_every
+        output_str += input_str[last_break:end_break+1] + "\n"
+        last_break = end_break + 1
+    return output_str
